8121bb16df02e508c300af424df467913939fe19
giacomo <giacomo@macro-eyes.com>
2021-10-03 16:04:10 +0200

    define val and test step when need to guess which labels does the current item belongs to (clustering)

--- src/train.py
+++ src/train.py
@@ -1,20 +1,23 @@
 import os
-from typing import Tuple, Union, Type
+from typing import Tuple, Union, Type, Optional
 import logging
 import multiprocessing as mp
 
 from torch.utils.data import DataLoader, random_split, ConcatDataset
 import torch
 import torch.nn.functional as nn_func
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 import torchvision
 from torchvision.datasets import FashionMNIST
 from torchvision import transforms
+from torchmetrics import Accuracy, Precision, Recall, MetricCollection
 import pytorch_lightning as pl
 from PIL import Image
 import numpy as np
 from torch import nn
 from fire import Fire
 from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor, ModelCheckpoint
+from tqdm.auto import tqdm
 
 LOGGER = logging.getLogger(__name__)
 DEV = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -95,12 +98,25 @@ class FashionMNISTDataModule(pl.LightningDataModule):
 
 class Model(pl.LightningModule):
     """"""
-    def __init__(self, lr: float, tau_plus: float = .1, beta: float = 1.0, features: int = 512):
+    def __init__(self,
+                 lr: float,
+                 ds_memory: Optional[DataLoader] = None,
+                 tau_plus: float = .1,
+                 beta: float = 1.0,
+                 features: int = 512,
+                 temperature: float = .5,
+                 num_classes: int = 10):
         """"""
         super().__init__()
         self.lr = lr
         self.beta = beta
         self.tau_plus = tau_plus
+        self.ds_memory = ds_memory
+        self.feature_labels = None
+        self.feature_bank = None
+        self.temperature = temperature
+        self.num_classes = num_classes
+        self.k_closest = 100
 
         # load base model
         base = torchvision.models.resnet18(pretrained=False, progress=True)
@@ -123,7 +139,7 @@ class Model(pl.LightningModule):
         )
 
         # loss and metrics
-        # self.metrics = MetricCollection([Accuracy(), Precision(), Recall()])
+        self.metrics = MetricCollection([Accuracy(), Precision(), Recall()])
         self.loss = Model.criterion
 
         # save hyperparameters
@@ -140,7 +156,7 @@ class Model(pl.LightningModule):
         return negative_mask
 
     @staticmethod
-    def criterion(out_1, out_2, tau_plus, beta, estimator: str = 'hard', temperature: float = .5):
+    def criterion(out_1, out_2, tau_plus, beta, temperature: float, estimator: str = 'hard'):
         """"""
         # neg score
         out = torch.cat([out_1, out_2], dim=0)
@@ -172,6 +188,21 @@ class Model(pl.LightningModule):
 
         return loss
 
+    def on_validation_start(self) -> None:
+        """Extract features for all labeled data in the training set (feature bank)"""
+        feature_bank, targets = [], []
+        for data, _, target in tqdm(self.ds_memory, desc='Feature extracting for bank', leave=False):
+            feature, _ = self(data.to(DEV, non_blocking=True))
+            feature_bank.append(feature)
+            targets += list(target)
+        self.feature_bank = torch.cat(feature_bank, dim=0).t().contiguous()
+        self.feature_labels = torch.tensor(targets, device=self.feature_bank.device)
+
+    def on_validation_end(self) -> None:
+        """Free variables"""
+        self.feature_bank = None
+        self.feature_labels = None
+
     def forward(self, input_tensor: torch.Tensor):
         """Forward pass"""
         x = self.encoder(input_tensor)
@@ -187,78 +218,69 @@ class Model(pl.LightningModule):
         # compute loss
         _, out_1 = self(im_1)
         _, out_2 = self(im_2)
-        loss = self.loss(out_1, out_2, self.tau_plus, self.beta)
+        loss = self.loss(out_1, out_2, self.tau_plus, self.beta, self.temperature)
 
         # log data
-        # _, class_idx = torch.max(preds, 1)
-        # metrics = {f'train_{key}': value for key, value in self.metrics(class_idx, y).items()}
         self.log_dict({'train_loss': loss}, on_step=False, on_epoch=True, prog_bar=False, logger=True)
-        # self.cache_class["train"] += [(pred.item(), gt.item()) for pred, gt in zip(class_idx, y)]
 
         return loss
 
     def configure_optimizers(self):
         """configure optimizers"""
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=1e-6)
-        # lr_scheduler = {'scheduler': ReduceLROnPlateau(optimizer, patience=10, factor=.5), "monitor": "train_loss"}
+        lr_scheduler = {'scheduler': ReduceLROnPlateau(optimizer, patience=10, factor=.5), "monitor": "train_loss"}
 
-        return optimizer
+        return [optimizer], [lr_scheduler]
+
+    def guess_label(self, batch) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Guess label based on embeddings similarity"""
+        # unpack batch
+        data, data_2, target = batch
+
+        # compute loss
+        feature, out = self(data)
+        _, out_2 = self(data_2)
+        loss = self.loss(out, out_2, self.tau_plus, self.beta, temperature=self.temperature)
+
+        # compute cos similarity between each feature vector and feature bank ---> [B, N]
+        sim_matrix = torch.mm(feature, self.feature_bank)  # [B, K]
+        sim_weight, sim_indices = sim_matrix.topk(k=self.k_closest, dim=-1)  # [B, K]
+        sim_labels = torch.gather(self.feature_labels.expand(data.size(0), -1), dim=-1, index=sim_indices)
+        sim_weight = (sim_weight / self.temperature).exp()
+
+        # counts for each class
+        one_hot_label = torch.zeros(data.size(0) * self.k_closest, self.num_classes, device=sim_labels.device)  # [B*K, C]
+        one_hot_label = one_hot_label.scatter(dim=-1, index=sim_labels.view(-1, 1).long(), value=1.0)  # weighted score ---> [B, C]
+        pred_scores = torch.sum(one_hot_label.view(data.size(0), -1, self.num_classes) * sim_weight.unsqueeze(dim=-1), dim=1)
+
+        return loss, pred_scores, target
 
     def validation_step(self, batch, batch_idx) -> torch.Tensor:
         """training step"""
-        # unpack batch
-        data, _, target = batch
+        loss, pred_scores, targets = self.guess_label(batch=batch)
 
-        # # compute loss
-        # _, out_1 = self(im_1)
-        # _, out_2 = self(im_2)
-        # loss = self.loss(out_1, out_2, self.tau_plus, self.beta)
-        #
-        # # log data
-        # self.log_dict({'val_loss': loss}, on_step=False, on_epoch=True, prog_bar=False, logger=True)
-        #
-        # return loss
+        # log data
+        _, class_idx = torch.max(pred_scores, 1)
+        metrics = {f'val_{key}': value for key, value in self.metrics(class_idx, targets).items()}
+        self.log_dict({'val_loss': loss, **metrics}, on_step=True, on_epoch=True, prog_bar=False, logger=True)
 
-        feature, out = self(data)
+        return loss
 
-        # compute cos similarity between each feature vector and feature bank ---> [B, N]
-        sim_matrix = torch.mm(feature, feature_bank)
-        # [B, K]
-        sim_weight, sim_indices = sim_matrix.topk(k=k, dim=-1)
-        # [B, K]
-        sim_labels = torch.gather(feature_labels.expand(data.size(0), -1), dim=-1, index=sim_indices)
-        sim_weight = (sim_weight / temperature).exp()
+    def test_step(self, batch, batch_idx) -> torch.Tensor:
+        """test step"""
+        loss, pred_scores, targets = self.guess_label(batch=batch)
 
-        # counts for each class
-        one_hot_label = torch.zeros(data.size(0) * k, c, device=sim_labels.device)
-        # [B*K, C]
-        one_hot_label = one_hot_label.scatter(dim=-1, index=sim_labels.view(-1, 1).long(), value=1.0)
-        # weighted score ---> [B, C]
-        pred_scores = torch.sum(one_hot_label.view(data.size(0), -1, c) * sim_weight.unsqueeze(dim=-1), dim=1)
+        # log data
+        _, class_idx = torch.max(pred_scores, 1)
+        metrics = {f'test_{key}': value for key, value in self.metrics(class_idx, targets).items()}
+        self.log_dict({'test_loss': loss, **metrics}, on_step=True, on_epoch=True, prog_bar=False, logger=True)
 
-        pred_labels = pred_scores.argsort(dim=-1, descending=True)
-        total_top1 += torch.sum((pred_labels[:, :1] == target.long().unsqueeze(dim=-1)).any(dim=-1).float()).item()
-        total_top5 += torch.sum((pred_labels[:, :5] == target.long().unsqueeze(dim=-1)).any(dim=-1).float()).item()
-        test_bar.set_description('KNN Test Epoch: [{}/{}] Acc@1:{:.2f}% Acc@5:{:.2f}%'
-                                 .format(epoch, epochs, total_top1 / total_num * 100, total_top5 / total_num * 100))
-
-    # def test_step(self, batch, batch_idx) -> torch.Tensor:
-    #     """It defines the test step"""
-    #     im_1, _, label = batch
-    #     preds = self(x)
-    #     loss = self.test_loss(input=preds, target=y)
-    #
-    #     # accuracy
-    #     _, class_idx = torch.max(preds, 1)
-    #     metrics = {f'test_{key}': value for key, value in self.metrics(class_idx, y).items()}
-    #     self.log_dict({'test_loss': loss, **metrics}, on_step=False, on_epoch=True, prog_bar=False, logger=False)
-    #
-    #     return loss
+        return loss
 
 
 class Finetuner(pl.LightningModule):
     """"""
-    def __init__(self, lr: float, pretrained_path: str, num_classes: int = 10):
+    def __init__(self, lr: float, pretrained_path: str, num_classes: int = 10, train_encoder: bool = False):
         """"""
         super().__init__()
         self.lr = lr
@@ -266,6 +288,8 @@ class Finetuner(pl.LightningModule):
         # load base model
         model = Model(lr=self.lr)
         model.load_state_dict(torch.load(pretrained_path, map_location=DEV))
+        for param in model.e.parameters():
+            param.requires_grad = train_encoder
 
         # encoder, classifier and loss
         self.encoder = model.encoder
@@ -286,7 +310,7 @@ class Finetuner(pl.LightningModule):
 class Cli:
     """Command line interface"""
     @staticmethod
-    def train_hcl(logs_dir: str = os.getcwd(), max_epochs: int = 300) -> None:
+    def train_hcl(logs_dir: str = os.getcwd(), max_epochs: int = 100) -> None:
         """
         Train binary classifier for solar panel detection
         :param logs_dir: where to save the logs
@@ -295,7 +319,7 @@ class Cli:
         """
         # initialize data and model
         ds = FashionMNISTDataModule(ds=FashionMNISTPair)
-        model = Model(lr=1e-3)
+        model = Model(lr=1e-3, ds_memory=DataLoader(ds.ds['train_labeled'], batch_size=16))
 
         # initialize callbacks
         callbacks = [
@@ -306,25 +330,12 @@ class Cli:
 
         # initialize trainer and run it
         gpus = torch.cuda.device_count()
-        trainer = pl.Trainer(default_root_dir=logs_dir, gpus=gpus, callbacks=callbacks, max_epochs=max_epochs)
+        trainer = pl.Trainer(default_root_dir=logs_dir, gpus=gpus, callbacks=callbacks, max_epochs=max_epochs, limit_train_batches=2)
         trainer.fit(model, ds)
 
     @staticmethod
     def finetune(logs_dir: str = os.getcwd(), max_epochs: int = 300) -> None:
         return NotImplementedError
-
-
-def testing():
-    ds = FashionMNISTDataModule(ds=FashionMNISTPair, batch_size=512)
-
-    for im, im2, label in ds.train_dataloader():
-        print(im.shape, im2.shape, label.shape)
-
-    for im, im2, label in ds.val_dataloader():
-        print(im.shape, label.shape)
-
-    for im, im2, label in ds.test_dataloader():
-        print(im.shape, label.shape)
 
 
 if __name__ == '__main__':
